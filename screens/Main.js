e86f07db4582baf6533cd0b21cd5c5521253b1c3
noct <ezy_95@outlook.com>
2022-08-11 10:33:56 +0800

    the list

--- screens/Main.js
+++ screens/Main.js
@@ -1,24 +1,49 @@
 import { useState, useContext } from 'react';
-import { VStack, Input, Text } from 'native-base';
+import {
+  VStack,
+  FormControl,
+  Input,
+  Button,
+  FlatList,
+  Text,
+} from 'native-base';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { GlobalAppContext } from '../context/globalAppContext';
 
 export default function Main() {
   const { dummyValue } = useContext(GlobalAppContext);
 
+  const [rushList, setRushList] = useState(['ikan', 'masin']);
   const [rushText, setRushText] = useState('mula');
 
   return (
     <>
       <VStack alignItems='center'>
-        <Input
-          onChangeText={(text) => setRushText(text)}
-          placeholder="Put what you're rushing for"
-          w='75%'
-          my='3'
+        <FormControl isRequired isInvalid={false} alignItems='center'>
+          <Input
+            onChangeText={(text) => setRushText(text)}
+            placeholder="Put what you're rushing for"
+            p='2'
+            my='3'
+            w='75%'
+          />
+          <FormControl.ErrorMessage mb='3'>
+            What are you rushing for?
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <Button
+          onPress={() => {
+            alert('hello');
+          }}
+          colorScheme='primary'
+        >
+          Add
+        </Button>
+        <FlatList
+          data={rushList}
+          renderItem={(singleRush) => <Text>{singleRush.item}</Text>}
         />
-        <Text>{rushText}</Text>
-        <Text>{dummyValue}</Text>
       </VStack>
     </>
   );
